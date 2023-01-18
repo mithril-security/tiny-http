84614fe724cff84a9912cadbf9e3108a236a099e
Corentin Lauverjat <corentin.lauverjat@mithrilsecurity.io>
2023-01-18 16:09:42 +0000

    temporary: add allow warning to avoid polluting the main crate with this crate warnings

--- src/lib.rs
+++ src/lib.rs
@@ -87,6 +87,7 @@
 //! # let response = tiny_http::Response::from_file(File::open(&Path::new("image.png")).unwrap());
 //! let _ = request.respond(response);
 //! ```
+#![allow(warnings)]
 #![forbid(unsafe_code)]
 #![deny(rust_2018_idioms)]
 #![allow(clippy::match_like_matches_macro)]
